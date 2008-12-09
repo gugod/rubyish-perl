63160deb4f05fb74cc4810c2a7d53f2a4ab9ab42
Liu Kang-min <gugod@gugod.org>
2008-12-09 12:47:19 +0800

    Document about Rubyish::Object

--- lib/Rubyish/Object.pm
+++ lib/Rubyish/Object.pm
@@ -4,6 +4,14 @@ use strict;
 
 Rubyish::Object - the parent class of all classes in Rubyish
 
+=head1 DESCRIPTION
+
+This implements the "Object" class in ruby.
+
+Baiscally if you are defining a Rubyish class, you should not use this
+class as the base class, but just say C<use Rubyish;> in your code.
+This is almost all for the internals of Rubyish Perl.
+
 =cut
 
 package Rubyish::Object;
@@ -16,7 +24,7 @@ use Rubyish::Kernel;
 
 =head2 new()
 
-Not documented
+The default constructor for all sub-classes.
 
 =cut
 
@@ -25,6 +33,12 @@ sub new {
     my $self = {};
     return bless $self, $class;
 }
+
+=head2 object_id(), __id__()
+
+Returns the internal address of the object_id
+
+=cut
 
 def object_id {
     refaddr $self;
@@ -56,13 +70,13 @@ sub is_a {
 
 { no strict; *kind_of = *is_a; }
 
-=head2 __send__
+=head2 __send__($name, @args)
 
 Invokes method by string
-    
+
     package Animal;
     use base Rubyish::Object;
-    
+
     def hello {
         my ($self, $arg) = @_;
         "hello, $arg.";
@@ -71,7 +85,7 @@ Invokes method by string
     1;
 
     my $dog = Animal->new;
-    print $dog->__send__("hello", "perl"); #=> "hello, perl."
+    print $dog->__send__("hello", "perl"); # "hello, perl."
 
 =cut
 
@@ -89,7 +103,7 @@ sub __send__ {
 
 =head2 to_yaml()
 
-Not Documented
+Serialize the object to YAML.
 
 =cut
 
@@ -98,9 +112,9 @@ sub to_yaml {
     return YAML::Dump(@_);
 }
 
-=head2
+=head2 methods()
 
-Return a list of names of methods
+Return a list of names of instance methods.
 
 =cut
 
@@ -110,7 +124,7 @@ def methods {
     Rubyish::Kernel::Array($methods);
 };
 
-=head2 inpsect
+=head2 inpsect()
 
 Returns a string containing a human-readable representation of obj.
 
@@ -121,9 +135,9 @@ def inspect {
     "#<" . ref($self) . ":" . $2 . ">";
 };
 
-=head2 ancestors #=> ancestors Array
+=head2 ancestors()
 
-All ancestors this class inherit
+All ancestors of this object.
 
 =cut
 
@@ -134,3 +148,41 @@ def ancestors {
 };
 
 1;
+
+=head1 AUTHOR
+
+Kang-min Liu  C<< <gugod@gugod.org> >>, shelling C<shelling@cpan.org>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2008, Kang-min Liu C<< <gugod@gugod.org> >>.
+
+This is free software, licensed under:
+
+    The MIT (X11) License
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
+
+
+
