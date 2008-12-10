dcab09b946fadeab99068407f551f01cb2c57dc1
Liu Kang-min <gugod@gugod.org>
2008-12-10 11:03:27 +0800

    Rubyish::Syntax::class now works under all packages.

--- lib/Rubyish/Syntax/class.pm
+++ lib/Rubyish/Syntax/class.pm
@@ -1,17 +1,29 @@
-package main;
-use strict;
-sub class { $_[0]->(); }
-
-use Devel::Declare;
-use Devel::Declare 'class' => [
-    DECLARE_PACKAGE,
-    sub {
-        my ($usepack, $use, $inpack, $name, $proto, $is_block) = @_;
-        return (sub (&) { shift; }, undef, "package ${name}; use Rubyish;");
-    }
-];
-
 package Rubyish::Syntax::class;
+use Devel::Declare;
+
+sub import {
+    my ($class) = @_;
+    my $caller = caller;
+
+    {
+        no strict;
+        *{"${caller}::class"} = sub { $_[0]->(); };
+    }
+
+    eval "package $caller; use Devel::Declare;";
+
+    Devel::Declare->setup_for(
+        $caller => {
+            'class' => [
+                DECLARE_PACKAGE,
+                sub {
+                    my ($usepack, $use, $inpack, $name, $proto, $is_block) = @_;
+                    return (sub (&) { shift; }, undef, "package ${name}; use Rubyish;");
+                }
+            ]
+        }
+    );
+};
 
 1;
 
